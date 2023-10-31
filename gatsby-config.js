53dac19e81533a6cca74a26d0c8c7905f3287c2a
armeniocardoso <armeniotorres@gmail.com>
2023-10-31 20:42:21 -0300

    Commit Inicial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,6 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
     title: `Estudos do Jamstack com Gatsby`,
-    siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
 }
